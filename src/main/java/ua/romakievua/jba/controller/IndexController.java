8e8d39286c925be6f9055ffa838fa1b2108dd97d
Roman Pryshchepa <roma.kiev.ua@gmail.com>
2016-11-23 20:24:18 +0200

    commit lesson 10

---
+++ src/main/java/ua/romakievua/jba/controller/IndexController.java
@@ -0,0 +1,13 @@
+package ua.romakievua.jba.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
